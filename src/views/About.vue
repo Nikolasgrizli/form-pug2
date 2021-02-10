4e064781d41f6c3ff685ad83d9361a6f83232676
Евсюков Николай Михайлович <nikolasgrizli@gmail.com>
2021-02-10 20:42:15 +0200

    start with pug & scss

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,15 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<template lang="pug">
+.test
+  h1 Test
+
+  #app
+    v-form(@submit='onSubmit')
+      v-field(name='name' type='text' placeholder='Who are you' :rules='isRequired')
+      error-message(name='name')
+      button Submit
 </template>
+
+<style lang="scss">
+@import "./../assets/scss/index.scss";
+
+</style>
